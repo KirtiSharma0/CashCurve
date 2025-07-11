2c0886b927cfb8fe5aead289c2b095402b1d80fb
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-11 19:25:14 +0530

    initial commit

--- frontend/cashcurve/src/pages/Auth/Login.jsx
+++ frontend/cashcurve/src/pages/Auth/Login.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-sm text-slate-700 mt-[5px] mb-6'>Please enter your details to log-in</p>
+      </div>
+    </AuthLayout>
   )
 }
 
