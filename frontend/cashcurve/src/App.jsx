f7304a0065aea1a150069d0a05201871f98c4eb5
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-11 19:01:29 +0530

    initial commit

--- frontend/cashcurve/src/App.jsx
+++ frontend/cashcurve/src/App.jsx
@@ -1,12 +1,43 @@
- import React from 'react'
- 
+ import React from 'react';
+ import{
+  BrowseRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+ } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
  const App = () => {
    return (
-     <div className='text-2xl'>
-       app
+     <div>
+        <Route>
+          <Routes>
+            <Route path="/" element={<Root/>} />
+            <Route path="/login" exact element={<Login/>} />
+            <Route path="/signup" exact element={<Signup/>} />
+            <Route path="/home" exact element={<Home/>} />
+            <Route path="/income" exact element={<Income/>} />
+            <Route path="/expense" exact element={<Expense/>} />
+          </Routes>
+        </Route>
      </div>
    )
  }
  
- export default App
+ export default App;
+
+const Root = ()=>{
+//check if token exist in local storage
+const isAuthenticated = !!localStorage.getItem("token");
+//redirect to dashboard if it is authenticated otherwise navigate to login page
+return isAuthenticated ? (
+  <Navigate to = "/dashboard"/>
+) : (
+  <Navigate to = "/login"/>
+)
+}
+
  
